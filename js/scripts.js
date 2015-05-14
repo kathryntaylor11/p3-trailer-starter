03fac75e947975fddc59ba1d3873fb2e43fd966f
Taylor <kathrynt@engr-134-12.eastern.ewu.edu>
2015-05-14 09:33:27 -0700

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
-// My Scripts
+// Media Element Player
+
+$('audio,video').mediaelementplayer({
+	success: function(player, node) {
+	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+	},
+	startLanguage: 'en',
+	translationSelector: true
+});
 
